bb8711063e226c3a24eda3c94496a123ea33280c
hkato <hiroharu.kato.1989.10.13@gmail.com>
2018-01-08 19:37:24 +0900

    setup

--- setup.py
+++ setup.py
@@ -3,5 +3,7 @@ import setuptools
 setuptools.setup(
     name='fast_gaussian_filter',
     version='0.0.1',
-    test_suite='tests'
+    test_suite='tests',
+    install_requires=['numpy', 'chainer', 'cupy'],
+    packages=['fast_gaussian_filter'],
 )

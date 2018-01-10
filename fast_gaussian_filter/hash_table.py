0fc40188e2103cc1473510159ea29b9822d2f50d
hkato <hiroharu.kato.1989.10.13@gmail.com>
2018-01-10 16:32:41 +0900

    ?

---
+++ fast_gaussian_filter/hash_table.py
@@ -0,0 +1,129 @@
+import string
+
+import chainer
+import cupy as cp
+
+
+class HashMap(object):
+    def __init__(self, data, table_size=2 ** 24):
+        xp = chainer.cuda.get_array_module(data)
+        self.hash_factor = 2531011
+        self.batch_size, self.num_points, self.dim = data.shape
+        self.table_size = table_size
+
+        self.indices = cp.ascontiguousarray(cp.zeros((self.batch_size, self.table_size,), 'int32')) - 1
+        self.values = cp.ascontiguousarray(cp.zeros((self.batch_size, self.table_size, self.dim), 'int32'))
+        self.value_list = cp.ascontiguousarray(cp.zeros((self.batch_size, self.table_size, self.dim), 'int32'))
+        self.size = None
+
+        self.init_keys(data)
+
+    def init_keys(self, data):
+        data = cp.ascontiguousarray(data)
+        used = cp.ascontiguousarray(cp.zeros((self.batch_size, self.table_size), 'int32'))
+        written = cp.ascontiguousarray(cp.zeros((self.batch_size, self.table_size), 'int32'))
+        count = cp.ascontiguousarray(cp.zeros((self.batch_size,), 'int32'))
+        loop_indices = cp.arange(data.size / self.dim).astype('int32')
+
+        chainer.cuda.elementwise(
+            'int32 j, raw int32 data, raw int32 indices, raw int32 values, ' +
+            'raw int32 value_list, raw int32 used, raw int32 written, raw int32 count',
+            '',
+            string.Template('''
+                int* value_init;
+                int* value;
+                value_init = &data[i * ${dim}];
+                int bn = i / ${num_points};
+
+                /* compute initial key */
+                unsigned int key = 0;
+                value = value_init;
+                for (int k = 0; k < ${dim}; k++) key = (key + *value++) * ${hash_factor};
+                key = key % ${table_size};
+
+                while (true) {
+                    /* check if the key is used */
+                    int ret;
+                    ret = used[bn * ${table_size} + key];
+                    if (ret == 0) ret = atomicExch(&used[bn * ${table_size} + key], 1);
+
+                    if (ret == 0) {
+                        /* register true key */
+                        int* value_ref = &values[(bn * ${table_size} + key) * ${dim}];
+                        value = value_init;
+                        for (int k = 0; k < ${dim}; k++) *value_ref++ = *value++;
+                        written[bn * ${table_size} + key] = 1;
+
+                        int num = atomicAdd(&count[bn], 1);
+                        indices[bn * ${table_size} + key] = num;
+
+                        value_ref = &value_list[(bn * ${table_size} + num) * ${dim}];
+                        value = value_init;
+                        for (int k = 0; k < ${dim}; k++) *value_ref++ = *value++;
+
+                        break;
+                    } else {
+                        bool match = true;
+                        while (atomicAdd(&written[bn * ${table_size} + key], 0) == 0) {}
+                        int* value_ref = &values[(bn * ${table_size} + key) * ${dim}];
+                        value = value_init;
+                        for (int k = 0; k < ${dim}; k++) if (*value_ref++ != *value++) match = false;
+                        if (match) {
+                            break;
+                        } else {
+                            key = (key + 1) % ${table_size};
+                        }
+                    }
+                }
+            ''').substitute(
+                table_size=self.table_size,
+                hash_factor=self.hash_factor,
+                num_points=self.num_points,
+                dim=self.dim,
+            ),
+            'kernel',
+        )(loop_indices, data, self.indices, self.values, self.value_list, used, written, count)
+        self.size = int(count.max())
+
+    def find(self, data):
+        ret = cp.ascontiguousarray(cp.zeros(data.shape[:-1], 'int32')) - 1
+        data = cp.ascontiguousarray(data)
+        loop_indices = cp.arange(data.size / self.dim).astype('int32')
+        chainer.cuda.elementwise(
+            'int32 j, raw int32 data, raw int32 indices, raw int32 values, raw int32 ret',
+            '',
+            string.Template('''
+                /* */
+                int* value = &data[j * ${dim}];
+                int bn = i / ${num_points};
+
+                /* compute initial key */
+                unsigned int key = 0;
+                for (int k = 0; k < ${dim}; k++) key = (key + value[k]) * ${hash_factor};
+                key = key % ${table_size};
+
+                while (1) {
+                    if (indices[bn * ${table_size} + key] < 0) {
+                        ret[j] = -1;
+                        break;
+                    }
+                    bool match = true;
+                    for (int k = 0; k < ${dim}; k++)
+                        if (values[(bn * ${table_size} + key) * ${dim} + k] != value[k])
+                            match = false;
+                    if (match) {
+                        ret[j] = indices[bn * ${table_size} + key];
+                        break;
+                    } else {
+                        key = (key + 1) % ${table_size};
+                    }
+                }
+            ''').substitute(
+                table_size=self.table_size,
+                hash_factor=self.hash_factor,
+                num_points=data.shape[1],
+                dim=self.dim,
+            ),
+            'function',
+        )(loop_indices, data, self.indices, self.values, ret)
+        return ret
---
+++ tests/test_gaussian_filter.py
@@ -0,0 +1,143 @@
+import time
+import unittest
+
+import chainer
+import chainer.gradient_check
+import numpy as np
+import scipy.misc
+
+import fast_gaussian_filter
+
+
+class TestFastGaussianFilter(unittest.TestCase):
+    def test_forward(self):
+        # reference time: 827 +- 4 ms
+        # this gpu computation: 104 +- 1 ms
+        std_spatial = 5
+        std_color = 0.125
+        image_in = scipy.misc.imread('./ref/in.jpg').astype('float32') / 255.
+        image_ref = np.array([[float(l2) for l2 in l.strip().split(',')] for l in open('./ref/ref.log').readlines()])
+        image_ref = image_ref.reshape(image_in.shape)
+        y, x = np.meshgrid(np.arange(image_in.shape[0]), np.arange(image_in.shape[1]), indexing='ij')
+        points = np.concatenate((x[:, :, None], y[:, :, None], image_in), axis=-1)
+        points[:, :, :2] /= std_spatial
+        points[:, :, 2:] /= std_color
+
+        points = points.reshape((1, -1, 5))
+        features = image_in.reshape((1, -1, 3))
+        points = chainer.cuda.to_gpu(points).astype('float32')
+        features = chainer.cuda.to_gpu(features).astype('float32')
+
+        ts = time.time()
+        image_out = fast_gaussian_filter.fast_gaussian_filter(features, points=points)
+        te = time.time()
+        print 'time (1)', te - ts
+
+        for i in range(10):
+            ts = time.time()
+            image_out = fast_gaussian_filter.fast_gaussian_filter(features, points=points)
+            image_out = image_out.reshape(image_ref.shape)
+            te = time.time()
+            print 'time (2-%d)' % i, te - ts
+
+        lattice = fast_gaussian_filter.Lattice(points)
+        for i in range(10):
+            ts = time.time()
+            image_out = fast_gaussian_filter.fast_gaussian_filter(features, lattice=lattice)
+            image_out = image_out.reshape(image_ref.shape)
+            te = time.time()
+            print 'time (3-%d)' % i, te - ts
+
+        diff = image_in - image_out.data.get()
+        print 'diff_in', np.square(diff).mean()
+        diff = image_ref - image_out.data.get()
+        print 'diff_out', np.square(diff).mean()
+
+        scipy.misc.toimage(image_out.data.get(), cmin=0, cmax=1).save('./ref/out.jpg')
+
+    def test_backward(self):
+        # reference time: 827 +- 4 ms
+        std_spatial = 5
+        std_color = 0.125
+        image_in = scipy.misc.imread('./ref/in.jpg').astype('float32') / 255.
+        y, x = np.meshgrid(np.arange(image_in.shape[0]), np.arange(image_in.shape[1]), indexing='ij')
+        points = np.concatenate((x[:, :, None], y[:, :, None], image_in), axis=-1)
+        points[:, :, :2] /= std_spatial
+        points[:, :, 2:] /= std_color
+
+        points = points.reshape((1, -1, 5))
+        features = image_in.reshape((1, -1, 3))
+        points = chainer.cuda.to_gpu(points).astype('float32')
+        features = chainer.cuda.to_gpu(features).astype('float32')
+
+        gy = np.random.normal(size=features.shape).astype('float32')
+        gy = chainer.cuda.to_gpu(gy)
+        lattice = fast_gaussian_filter.Lattice(points)
+        function = fast_gaussian_filter.FastGaussianFilter(lattice)
+        chainer.gradient_check.check_backward(function, features, gy, eps=1e-1, atol=1e-3, rtol=1e-3)
+
+    def test_forward2(self):
+        # reference time: 827 +- 4 ms
+        # this gpu computation: 104 +- 1 ms
+        std_spatial = 5
+        std_color = 0.125
+        image_in = scipy.misc.imread('./ref/in.jpg').astype('float32') / 255.
+        image_ref = np.array([[float(l2) for l2 in l.strip().split(',')] for l in open('./ref/ref.log').readlines()])
+        image_ref = image_ref.reshape(image_in.shape)
+        y, x = np.meshgrid(np.arange(image_in.shape[0]), np.arange(image_in.shape[1]), indexing='ij')
+        points = np.concatenate((x[:, :, None], y[:, :, None], image_in), axis=-1)
+        points[:, :, :2] /= std_spatial
+        points[:, :, 2:] /= std_color
+
+        points = points.reshape((1, -1, 5))
+        features = image_in.reshape((1, -1, 3))
+        points = np.tile(points, (2, 1, 1))
+        features = np.tile(features, (2, 1, 1))
+        points[0] = 0
+        features[0] = 0
+        points = chainer.cuda.to_gpu(points).astype('float32')
+        features = chainer.cuda.to_gpu(features).astype('float32')
+
+        image_out = fast_gaussian_filter.fast_gaussian_filter(features, points=points)
+        image_out = image_out.reshape((2, image_in.shape[0], image_in.shape[1], image_in.shape[2]))[1]
+
+        diff = image_in - image_out.data.get()
+        print 'diff_in', np.square(diff).mean()
+        diff = image_ref - image_out.data.get()
+        print 'diff_out', np.square(diff).mean()
+
+    def test_forward3(self):
+        # high-dimension
+        dim = 64
+        std_spatial = 5
+        std_color = 0.125
+        image_in = scipy.misc.imread('./ref/in.jpg').astype('float32') / 255.
+        image_in = image_in[:256, :256]
+        y, x = np.meshgrid(np.arange(image_in.shape[0]), np.arange(image_in.shape[1]), indexing='ij')
+        points = np.concatenate((x[:, :, None], y[:, :, None], image_in), axis=-1)
+        points[:, :, :2] /= std_spatial
+        points[:, :, 2:] /= std_color
+
+        points = points.reshape((1, -1, 5))
+        features = np.random.random(size=(image_in.shape[0], image_in.shape[1], dim))
+        features = features.reshape((1, -1, features.shape[-1]))
+        points = np.tile(points, (2, 1, 1))
+        features = np.tile(features, (2, 1, 1))
+        points[0] = 0
+        features[0] = 0
+        points = chainer.cuda.to_gpu(points).astype('float32')
+        features = chainer.cuda.to_gpu(features).astype('float32')
+
+        lattice = fast_gaussian_filter.Lattice(points, hash_size=2 ** 20)
+        features_out = fast_gaussian_filter.fast_gaussian_filter(features, points=points)
+        print features_out[0, 0, 0]
+
+        ts = time.time()
+        features_out = fast_gaussian_filter.fast_gaussian_filter(features, points=points)
+        print features_out[0, 0, 0]
+        te = time.time()
+        print dim, te - ts
+
+
+if __name__ == '__main__':
+    unittest.main()
